3007dc6cb8014dd3764eea099d4e7af9273cfe38
slidebrass <slidebrass@gmail.com>
2024-04-01 10:37:22 -0500

    new backend endpoint

--- src/api/server.ts
+++ src/api/server.ts
@@ -1,5 +1,5 @@
 const token = '9bcd1983-9357-48cc-8a15-b7864cddfd4c'
-const endpoint = 'http://127.0.0.1:5000'
+const endpoint = 'https://flask-dog.onrender.com'
 
 export const server_calls = {
   // breed_info routes
